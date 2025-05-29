ba99d02368a6412b9b029e378447e345157b7241
fahh7 <fahh7@FREIMYHP>
2025-05-28 20:45:19 -0400

    Configurando inyeccion de dependencia Hilt para el parcial.

---
+++ app/src/main/java/edu/ucne/freimyhidalgo_p1_ap2/FreimyHidalgo_P1_AP2.kt
@@ -0,0 +1,8 @@
+package edu.ucne.freimyhidalgo_p1_ap2
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FreimyHidalgo_P1_AP2: Application() {
+}
--- app/src/main/java/edu/ucne/freimyhidalgo_p1_ap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/freimyhidalgo_p1_ap2/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.freimyhidalgo_p1_ap2.ui.theme.FreimyHidalgo_P1_AP2Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
