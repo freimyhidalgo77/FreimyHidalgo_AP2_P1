6984c1e9fc90d6a1c370053ef5b12767cd13e48e
fahh7 <fahh7@FREIMYHP>
2025-06-04 18:58:07 -0400

    App Module del proyecto

--- app/src/main/java/edu/ucne/freimyhidalgo_p1_ap2/data/di/AppModule.kt
+++ app/src/main/java/edu/ucne/freimyhidalgo_p1_ap2/data/di/AppModule.kt
@@ -1,4 +1,38 @@
 package edu.ucne.freimyhidalgo_p1_ap2.data.di
 
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.freimyhidalgo_p1_ap2.data.local.database.TareaDb
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
 object AppModule {
+
+    @Provides
+    @Singleton
+
+    fun TareaProvideDb(@ApplicationContext applicationContext: Context): TareaDb =
+        Room.databaseBuilder(
+            applicationContext,
+            TareaDb::class.java,
+            "TareaDb"
+        ).fallbackToDestructiveMigration().build()
+
+    @Provides
+    @Singleton
+
+    fun TareaProvideDao(tareaDBO:TareaDb) = tareaDBO.tareaDo()
+
 }
+
+
+
+
+
