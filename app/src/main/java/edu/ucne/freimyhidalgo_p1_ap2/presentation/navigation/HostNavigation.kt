d1471b4d93ab90a491953cb86715dc84b7b3c335
fahh7 <fahh7@FREIMYHP>
2025-06-04 18:58:20 -0400

    Navegacion a pantallas

--- app/src/main/java/edu/ucne/freimyhidalgo_p1_ap2/presentation/navigation/HostNavigation.kt
+++ app/src/main/java/edu/ucne/freimyhidalgo_p1_ap2/presentation/navigation/HostNavigation.kt
@@ -1,17 +1,20 @@
 package edu.ucne.freimyhidalgo_p1_ap2.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import edu.ucne.freimyhidalgo_p1_ap2.presentation.sistemas.SistemaListScreen
+import edu.ucne.freimyhidalgo_p1_ap2.data.local.database.TareaDb
+import edu.ucne.freimyhidalgo_p1_ap2.presentation.tareas.SistemaListScreen
+import edu.ucne.freimyhidalgo_p1_ap2.repository.TareaRepository
+import kotlinx.coroutines.CoroutineScope
 
 @Composable
-fun HostNavigation(
-    navHostController: NavHostController,
+fun HostNavigation(tareaDb:TareaDb,navHostController: NavHostController) {
+    val scope:CoroutineScope
+    val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner
+    val tareaRepository:TareaRepository
 
-    ) {
     NavHost(
         navController = navHostController,
         startDestination = Screen.List
