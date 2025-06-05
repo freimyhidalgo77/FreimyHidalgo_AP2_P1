4dc532e2ccc76f3d3326fd92a85c074a88ef79b2
fahh7 <fahh7@FREIMYHP>
2025-06-04 20:18:55 -0400

    HostNavigation

--- app/src/main/java/edu/ucne/freimyhidalgo_p1_ap2/presentation/navigation/HostNavigation.kt
+++ app/src/main/java/edu/ucne/freimyhidalgo_p1_ap2/presentation/navigation/HostNavigation.kt
@@ -1,26 +1,77 @@
 package edu.ucne.freimyhidalgo_p1_ap2.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import edu.ucne.freimyhidalgo_p1_ap2.data.local.database.TareaDb
-import edu.ucne.freimyhidalgo_p1_ap2.presentation.tareas.SistemaListScreen
+import edu.ucne.freimyhidalgo_p1_ap2.presentation.tareas.TareaDeleteScreen
+import edu.ucne.freimyhidalgo_p1_ap2.presentation.tareas.TareaEditBodyScreen
+import edu.ucne.freimyhidalgo_p1_ap2.presentation.tareas.TareaEditScreen
+import edu.ucne.freimyhidalgo_p1_ap2.presentation.tareas.TareaListScreen
+import edu.ucne.freimyhidalgo_p1_ap2.presentation.tareas.TareaScreen
 import edu.ucne.freimyhidalgo_p1_ap2.repository.TareaRepository
 import kotlinx.coroutines.CoroutineScope
 
 @Composable
-fun HostNavigation(tareaDb:TareaDb,navHostController: NavHostController) {
-    val scope:CoroutineScope
+
+fun HostNavigation(taraeDb:TareaDb, navHostController: NavHostController){
+
+    val scope = rememberCoroutineScope()
     val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner
-    val tareaRepository:TareaRepository
+    val tareaRepository: TareaRepository
 
     NavHost(
         navController = navHostController,
-        startDestination = Screen.List
+        startDestination = Screen.TareaList
     ) {
-        composable<Screen.List>{
-            SistemaListScreen()
+        composable<Screen.TareaList>{
+            TareaListScreen(
+                scope = scope,
+                onCreate = {
+                    navHostController.navigate(Screen.Tareas(0))
+                },
+                onEdit = {tarea->
+                    navHostController.navigate(Screen.EditTareaScreen(tarea))
+                },
+
+                onDelete = {tarea->
+                    navHostController.navigate(Screen.DeleteTareaScreen(tarea))
+
+                }
+            )
+        }
+
+        composable<Screen.Tareas>{
+            val args = it.toRoute<Screen.Tareas>()
+            TareaScreen(
+                goBack = {
+                    navHostController.navigateUp()
+                }
+            )
+        }
+
+        composable<Screen.EditTareaScreen>{
+            val args = it.toRoute<Screen.EditTareaScreen>()
+            TareaEditScreen(
+                tareaId = args.tareaId!!,
+                goBack = {
+                    navHostController.navigateUp()
+                }
+            )
+        }
+
+        composable<Screen.DeleteTareaScreen>{
+            val args = it.toRoute<Screen.DeleteTareaScreen>()
+            TareaDeleteScreen (
+                tareaId = args.tareaId!!,
+                goBack = {
+                    navHostController.navigateUp()
+
+                }
+            )
         }
     }
 }
